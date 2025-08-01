3e044d5df0e93cdb9109ea24e384d4607462a72b
katsuroom <kerrance.dai@gmail.com>
2025-08-01 15:58:57 -0400

    Remove cors from build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const removeCrossorigin = () => {
+  return {
+    name: 'remove-crossorigin-attribute',
+    transformIndexHtml(html) {
+      return html.replaceAll(`type="module" crossorigin`, "defer").replaceAll(`crossorigin`, "");
+    }
+  };
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeCrossorigin()],
   server: {
     port: 3000
-  }
+  },
+  base: "",    // use relative paths for assets,
 })
